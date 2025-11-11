4c48c0944303e0c9e310f5163200499d7923672b
osklc <osklcmail@gmail.com>
2025-11-11 23:45:55 +0300

    add login function

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ typedef struct
 	int account_num;
 	char account_name[MAX_LENGTH];
 	char account_surname[MAX_LENGTH];
-	int account_password;
+	char account_password[MAX_LENGTH];
 	float account_balance;
 } accountInformation;
 
@@ -46,7 +46,7 @@ int loadingScreen()
 	int r, q;
 	gotoxy(55,12);
 	printf("Loading...\n");
-	gotoxy(50,14);
+	gotoxy(49,14);
 	for(r=1;r<=20;r++)
 	{
 		for(q=0; q<=100000000;q++);
@@ -66,31 +66,49 @@ void adminTerminal()
 
 void createAccount()
 {
-	cleanScreen();
-	loadingScreen();
-	cleanScreen();
-	
-	accountInformation accounts[MAX_LENGTH];
-	
-	char unverified_name[MAX_LENGTH];
-	char unverified_surname[MAX_LENGTH];
-	char unverified_password[MAX_LENGTH];
-	
-	printf("\x1b[1m\x1b[35mAccount Creation Screen\x1b[0m\n");
-	
-	printf("\x1b[33mWARNING: \x1b[0mPlease use only English characters!\n");
-	printf("Please enter your name: ");
-	fgets(unverified_name,MAX_LENGTH,stdin);newLineClear(unverified_name);
-	printf("Please enter your surname: ");	
-	fgets(unverified_surname,MAX_LENGTH,stdin);newLineClear(unverified_surname);
-	printf("Please create a password: ");
-	fgets(unverified_password, MAX_LENGTH, stdin);newLineClear(unverified_password);
-	
-	FILE *fp = fopen(DATA_FILE,"a");
-	fprintf(fp,"Name: %s Surname: %s Password: %d\n",unverified_name,unverified_surname,atoi(unverified_password));
-	fclose(fp);
-	cleanScreen();
-	firstIntroduction();
+    cleanScreen();
+    loadingScreen();
+    cleanScreen();
+    
+    // Kullanacaðýmýz hesabý oluþtur
+    accountInformation new_account;
+    
+    // Kullanýcýdan bilgileri al
+    printf("\x1b[1m\x1b[35mAccount Creation Screen\x1b[0m\n");
+    printf("\x1b[33mWARNING: \x1b[0mPlease use only English characters!\n");
+    
+    printf("Please enter your name: ");
+    fgets(new_account.account_name, MAX_LENGTH, stdin);
+    newLineClear(new_account.account_name);
+    
+    printf("Please enter your surname: ");   
+    fgets(new_account.account_surname, MAX_LENGTH, stdin);
+    newLineClear(new_account.account_surname);
+    
+    printf("Please create a password: ");
+    fgets(new_account.account_password, MAX_LENGTH, stdin);
+    newLineClear(new_account.account_password);
+
+    // TODO: Bakiye ve hesap numarasý da burada atanabilir
+    new_account.account_balance = 0.0; 
+    // new_account.account_num = ... (Bunu oluþturmak için ayrý bir mantýk gerekir)
+
+    // Dosyayý "append binary" (ikili ekle) modunda aç
+    FILE *fp = fopen(DATA_FILE, "ab"); 
+    if (fp == NULL) {
+        printf("Error opening data file!\n");
+        return;
+    }
+
+    // fprintf yerine struct'ýn tamamýný dosyaya yaz
+    fwrite(&new_account, sizeof(accountInformation), 1, fp);
+    
+    fclose(fp);
+    
+    printf("Account created successfully!\n");
+    Sleep(1500);
+    cleanScreen();
+    firstIntroduction();
 }
 
 void loginAccount()
@@ -98,8 +116,61 @@ void loginAccount()
 	cleanScreen();
 	loadingScreen();
 	cleanScreen();
-	
 	printf("\x1b[1m\x1b[35mAccount Log In Screen\x1b[0m\n");
+	
+	char input_name[MAX_LENGTH];
+    char input_surname[MAX_LENGTH];
+    char input_password[MAX_LENGTH];
+	
+	printf("Please enter your name: ");
+    fgets(input_name, MAX_LENGTH, stdin);newLineClear(input_name);
+
+    printf("Please enter your surname: ");
+    fgets(input_surname, MAX_LENGTH, stdin);newLineClear(input_surname);
+
+    printf("Please enter your password: ");
+    fgets(input_password, MAX_LENGTH, stdin);newLineClear(input_password);
+	
+	
+	FILE *fp = fopen(DATA_FILE,"rb");
+	if (fp == NULL)
+    {
+        printf("\x1b[1m\x1b[31mERROR: \x1b[0mAccount data file not found.\n");
+        sleep(2);
+        firstIntroduction();
+        return;
+    }
+    
+    accountInformation check_account;
+    int login_success = 0;
+    
+    while(fread(&check_account,sizeof(accountInformation),1,fp) == 1)
+    {
+    	if(strcmp(input_name, check_account.account_name) == 0 
+		&& strcmp(input_surname, check_account.account_surname) == 0 
+		&& strcmp(input_password, check_account.account_password) == 0)
+		{
+			login_success = 1;
+			break;
+		}
+	}
+	fclose(fp);
+	cleanScreen();
+	
+	if(login_success)
+	{
+		printf("\x1b[1m\x1b[32mLogin Successful!\x1b[0m\n");
+        printf("Welcome, %s %s.\n", input_name, input_surname);
+        sleep(3);
+        //DEVAM EDECEK
+	}
+	else
+	{
+		printf("\x1b[1m\x1b[31mLogin Failed: \x1b[0mIncorrect name, surname, or password.\n");
+		sleep(2);
+		firstIntroduction();
+	}
+	
 }
 
 void firstIntroduction()
@@ -122,6 +193,7 @@ void firstIntroduction()
 	{
 		cleanScreen();
 		printf("\x1b[1m\x1b[31mERROR: \x1b[0mPlease enter a valid value!\n");
+		sleep(2);
 		firstIntroduction();
 	}
 	
